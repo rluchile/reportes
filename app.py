0b017a3a746fce93c583922cb245cf9187fc6f9d
rluchile <rluchile@gmail.com>
2025-05-02 13:24:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ with st.form("formulario_servicio"):
         fecha_llamado = st.date_input("Fecha Llamado")
         fecha_servicio = st.date_input("Fecha Servicio")
     with col2:
-        hora_llamado = st.time_input("Hora Llamado")
-        hora_inicio = st.time_input("Hora Inicio")
+        hora_llamado = st.time_input("Hora Llamado", value=datetime.time(9, 0))
+        hora_inicio = st.time_input("Hora Inicio", value=datetime.time(12, 0))
 
     modelo = st.text_input("Modelo")
     version = st.text_input("Versión Software")
